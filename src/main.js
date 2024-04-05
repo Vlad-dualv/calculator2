4718a93af0dee16b9af6d4a39509acfe8d6fe36f
Vlad <vladswork95@gmail.com>
2024-04-05 16:44:27 +0100

    Done

---
+++ src/main.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    if (display.value === "Error") {
+        display.value = input;
+    } else {
+        display.value += input;
+    }
+}
+
+function calculate () {
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "Error";
+    }
+}
+
+function clearDisplay () {
+    display.value = "";
+}
